9ccdcf712395a6b793c486b9c0d1bdc30b5e16b0
Uros Radujko <urosradujko@gmail.com>
2026-03-27 15:33:30 +0100

    Task 6

--- 6-mocking/api_client.py
+++ 6-mocking/api_client.py
@@ -2,16 +2,11 @@ import datetime
 
 
 def fetch_weather_data(city):
-    return {
-        "city": city,
-        "temp": 20,
-        "condition": "sunny",
-        "humidity": 60,
-    }
+    raise NotImplementedError("Implement fetch_weather_data using TDD")
 
 
 def fetch_forecast(city, days=3):
-    return [{"day": i + 1, "temp": 20 + i, "condition": "sunny"} for i in range(days)]
+    raise NotImplementedError("Implement fetch_forecast using TDD")
 
 
 def get_current_hour():
--- 6-mocking/weather_service.py
+++ 6-mocking/weather_service.py
@@ -2,22 +2,16 @@ import api_client
 
 
 def get_weather(city):
-    return api_client.fetch_weather_data(city)
+    raise NotImplementedError("Implement get_weather using TDD")
 
 
 def get_forecast(city, days=3):
-    return api_client.fetch_forecast(city, days)
+    raise NotImplementedError("Implement get_forecast using TDD")
 
 
 def is_good_weather(conditions):
-    return conditions in ("sunny", "partly cloudy")
+    raise NotImplementedError("Implement is_good_weather using TDD")
 
 
 def get_greeting_based_on_time():
-    hour = api_client.get_current_hour()
-    if 5 <= hour < 12:
-        return "Good morning"
-    elif 12 <= hour < 18:
-        return "Good afternoon"
-    else:
-        return "Good evening"
+    raise NotImplementedError("Implement get_greeting_based_on_time using TDD")
